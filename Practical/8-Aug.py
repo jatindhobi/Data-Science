0072796d1976752b69fe2c78bcd14a3bf117eaad
jatindhobi <131326165+jatindhobi@users.noreply.github.com>
2024-08-08 09:22:35 +0530

    init

--- Practical/8-Aug.py
+++ Practical/8-Aug.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import pickle as pk 
@@ -10,7 +9,7 @@ x=df[["age"]]
 y=df[["platelets"]]
 
 x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.2,train_size=0.8,random_state=50)
-model=LinearRegression()
+model=LinearRegression
 
 model.fit(x_train,y_train)
 
