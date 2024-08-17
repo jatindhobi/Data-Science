74337c3f34cdb623faf14b058829a1cafebdca19
jatindhobi <131326165+jatindhobi@users.noreply.github.com>
2024-08-17 09:30:49 +0530

    init

--- Practical/8-Aug.py
+++ Practical/8-Aug.py
@@ -1,19 +1,34 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-import pickle as pk 
+from sklearn.neighbors import KNeighborsClassifier
+import joblib
+import pickle
+import streamlit as st
 
+df=pd.read_csv(r"C:\5th Sem\Data-Science\Datasets\Iris.csv")
+x=df.drop(["Species","Id"],axis=1)
+y=df["Species"]
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=42)
 
-df=pd.read_csv(r"C:\5th Sem\Data-Science\Datasets\heart_failure_clinical_records_dataset.csv")
-x=df[["age"]]
-y=df[["platelets"]]
+knn=KNeighborsClassifier(n_neighbors=3)
+knn.fit(x_train,y_train)
+y_pred=knn.predict(x_test)
 
-x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.2,train_size=0.8,random_state=50)
-model=LinearRegression
-
-model.fit(x_train,y_train)
-
-pickle_out = open("pickle_file.pkl","wb")
-
-pk.dump(model,pickle_out)
+pickle_out=open("classifier.pkl","wb")
+pickle.dump(knn,pickle_out)
 pickle_out.close()
+
+st.markdown('## Iris Species Prediction')
+Sepal_length=st.number_input("Sepal Length(cm)")
+Sepal_width=st.number_input("Sepal Width (cm)")
+petal_length=st.number_input("petal Length(Cm)")
+petal_width=st.number_input("Petal Width (Cm)")
+if st.button('Predict'):
+    model=joblib.load("classifier.pkl")
+    x=np.array([Sepal_length,Sepal_width,petal_length,petal_width])
+    if any(x<=0):
+        st.markdown("### Input must be greater than 0")
+    else:
+        st.markdown(f'### Prediction Is {model.predict([[Sepal_length,Sepal_width,petal_length,petal_width]])}')
